9f9f199898256f09b5cec295c7a9a3e52920fd83
ShahHussainBangash <onlinesoftteach@gmail.com>
2019-05-22 20:52:33 +0500

    first commit

---
+++ app/src/main/java/com/example/shahbangash/suitgooglemap/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.shahbangash.suitgooglemap;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        map =  googleMap;
+        LatLng Pakistan =  new LatLng(-33.852, 151.211);
+        map.addMarker(new MarkerOptions().position(Pakistan).title("Pakistan"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Pakistan));
+
+
+    }
+}
